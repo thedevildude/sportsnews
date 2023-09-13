f19d4fc8ec4becca5e4b08b7e732e2344d38a13f
Devdeep Ghosh <devarmforce01@gmail.com>
2023-09-13 07:11:32 +0530

    users can view live news based on sports preferences

--- src/components/articles/ArticleListItems.tsx
+++ src/components/articles/ArticleListItems.tsx
@@ -4,6 +4,7 @@ import { sports } from "../../utils/api";
 import { ArticleListState, Sport } from "../../context/articles/types";
 import { useArticleListState } from "../../context/articles/context";
 import ArticleListCard from "./ArticleListCard";
+import { usePreferencesState } from "../../context/preferences/context";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -12,6 +13,7 @@ function classNames(...classes: string[]) {
 export default function ArticleListItems() {
   const articleListState: ArticleListState = useArticleListState();
   const [sportsData, setSportsData] = useState<Sport[]>([]);
+  const preferencesState = usePreferencesState();
 
   useEffect(() => {
     const fetchSports = async () => {
@@ -37,6 +39,14 @@ export default function ArticleListItems() {
     );
   }
 
+  // Filter articles based on selected sports or show all articles
+  const filteredArticles =
+    preferencesState.preferences.sports.length > 0
+      ? articleListState.articles.filter((article) =>
+          preferencesState.preferences.sports.includes(article.sport.name)
+        )
+      : articleListState.articles;
+
   return (
     <div className="w-full">
       <Tab.Group>
@@ -55,43 +65,74 @@ export default function ArticleListItems() {
           >
             Your News
           </Tab>
-          {sportsData.map((sports) => (
-            <Tab
-              key={sports.id}
-              className={({ selected }) =>
-                classNames(
-                  "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
-                  "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
-                  selected
-                    ? "bg-white shadow"
-                    : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
-                )
-              }
-            >
-              {sports.name}
-            </Tab>
-          ))}
+          {preferencesState.preferences.sports.length <= 0
+            ? sportsData.map((sports) => (
+                <Tab
+                  key={sports.id}
+                  className={({ selected }) =>
+                    classNames(
+                      "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                      "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                      selected
+                        ? "bg-white shadow"
+                        : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                    )
+                  }
+                >
+                  {sports.name}
+                </Tab>
+              ))
+            : preferencesState.preferences.sports.map((sport) => (
+                <Tab
+                  key={sportsData.find((sports) => sports.name === sport)?.id}
+                  className={({ selected }) =>
+                    classNames(
+                      "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                      "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                      selected
+                        ? "bg-white shadow"
+                        : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                    )
+                  }
+                >
+                  {sport}
+                </Tab>
+              ))}
         </Tab.List>
         <Tab.Panels className="mt-2">
           {/* Tab panel for "Your News */}
           <Tab.Panel>
             <div className="flex flex-col gap-2">
-              {articleListState.articles.map((article) => (
+              {filteredArticles.map((article) => (
                 <ArticleListCard key={article.id} {...article} />
               ))}
             </div>
           </Tab.Panel>
-          {sportsData.map((sports) => (
-            <Tab.Panel key={sports.id}>
-              <div className="flex flex-col gap-2">
-                {articleListState.articles
-                  .filter((article) => article.sport.id === sports.id)
-                  .map((article) => (
-                    <ArticleListCard key={article.id} {...article} />
-                  ))}
-              </div>
-            </Tab.Panel>
-          ))}
+          {preferencesState.preferences.sports.length <= 0
+            ? sportsData.map((sports) => (
+                <Tab.Panel key={sports.id}>
+                  <div className="flex flex-col gap-2">
+                    {filteredArticles
+                      .filter((article) => article.sport.name === sports.name)
+                      .map((article) => (
+                        <ArticleListCard key={article.id} {...article} />
+                      ))}
+                  </div>
+                </Tab.Panel>
+              ))
+            : preferencesState.preferences.sports.map((sport) => (
+                <Tab.Panel
+                  key={sportsData.find((sports) => sports.name === sport)?.id}
+                >
+                  <div className="flex flex-col gap-2">
+                    {filteredArticles
+                      .filter((article) => article.sport.name === sport)
+                      .map((article) => (
+                        <ArticleListCard key={article.id} {...article} />
+                      ))}
+                  </div>
+                </Tab.Panel>
+              ))}
         </Tab.Panels>
       </Tab.Group>
     </div>
