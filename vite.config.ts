4f426da0a647d5b43c3fcf3cb1cf022db0aa25e8
Devdeep Ghosh <devarmforce01@gmail.com>
2023-09-14 06:58:27 +0530

    PWA Added

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default ({ mode }) => {
   return defineConfig({
@@ -7,7 +8,49 @@ export default ({ mode }) => {
       outDir: 'build',
     },
     plugins: [
-      react(),
+      react(), VitePWA({
+        devOptions: {
+          enabled: true,
+        },
+        manifest: {
+          name: "Sports Center Application",
+          short_name: "Sports Center",
+          description: "Sports Center Application",
+          icons: [
+            {
+              src: "/icons/android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/icons/android-chrome-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any maskable",
+            },
+            {
+              src: "icons/apple-touch-icon.png",
+              sizes: "180x180",
+              type: "image/png",
+            },
+            {
+              src: "icons/favicon-16x16.png",
+              sizes: "16x16",
+              type: "image/png",
+            },
+            {
+              src: "icons/favicon-32x32.png",
+              sizes: "32x32",
+              type: "image/png",
+            },
+            {
+              src: "icons/favicon.ico",
+              sizes: "48x48",
+              type: "image/png",
+            },
+          ],
+
+  }}),
     ],
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
